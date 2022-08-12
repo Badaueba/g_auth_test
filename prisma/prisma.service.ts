c996acc397843667a4c52c8d54ee09c90392e229
Alan Alves <alan.alves@pinpeople.com.br>
2022-08-12 10:36:15 -0300

    feat(prisma middleware): adding middleware to test prisma middleware

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -4,9 +4,18 @@ import { PrismaClient } from "@prisma/client";
 export class PrismaService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
         await this.$connect();
+
+        this.$use(async (params, next) => {
+            const result  = await next(params);
+            console.log('middleware', params.action);
+            return result;
+        })
     }
 
+    
+
     async enableShutdownHooks(app: INestApplication) {
+        
         this.$on('beforeExit', async ()=> 
             await app.close()
         )
